9b3e51006b074439a0069ae0499c83b87f7889c8
Tom Durrant <t.durrant@metocean.co.nz>
2018-02-15 23:00:13 +1300

    Tweaks to board sync tests

--- tests/test_board_sync.py
+++ tests/test_board_sync.py
@@ -1,20 +1,15 @@
 import unittest
-from trellowarrior import sync_tagged_cards, parse_config
+from trellowarrior import link_tagged_cards, parse_config
 
 class TestSync(unittest.TestCase):
 
     def test(self):
-        board_name_src='trellotest'
-        board_name_dest='trellotest2'
-        todo_list_name_src='To Do'
-        doing_list_name_src='Doing'
-        done_list_name_src='Done'
-        todo_list_name_dest='To Do'
-        doing_list_name_dest='Doing'
-        done_list_name_dest='Done'
+        board_name = {'src': 'trellotest', 'dest': 'trellotest2'}
+        todo_list_name = {'src': 'To Do', 'dest': 'To Do'}
+        doing_list_name = {'src': 'Doing', 'dest': 'Doing'}
+        done_list_name = {'src': 'Done', 'dest': 'Done'}
         sync_tag = 'sync'
-        sync_tagged_cards(board_name_src, todo_list_name_src, doing_list_name_src, done_list_name_src,
-                      board_name_dest, todo_list_name_dest, doing_list_name_dest, done_list_name_dest,
+        link_tagged_cards(board_name, todo_list_name, doing_list_name, done_list_name,
                      sync_tag)
 
 
